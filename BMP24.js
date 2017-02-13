fcc7a94da0a2c5fe9885fd2eeeafd6e89f8a5f7d
zengming <243786753@qq.com>
2017-02-13 13:05:37 +0800

    init

---
+++ BMP24.js
@@ -0,0 +1,246 @@
+var fs = require('fs');
+
+//初始版本确定于：20161103 18:31
+
+//学历不代表一个人的一切，这份代码不授权给看学历的公司使用
+
+function makeHeader(buf, fileLen, dataLen, w, h) {
+    var header = buf;
+    header.write("BM");
+    header.writeInt32LE(fileLen,2);//BMP文件大小
+    header.writeInt8(122,6);//特殊标识
+    header.writeInt32LE(54,10);
+    header.writeInt32LE(40,14);
+    header.writeInt32LE(w,18);//w
+    header.writeInt32LE(h,22);//h
+    header.writeInt16LE(1,26);
+    header.writeInt16LE(24,28);
+    header.writeInt32LE(dataLen,34);//数据大小
+}
+
+//构造函数
+function BMP24(w, h){
+    /*BMP数据的每一行必需能被4整除
+     例如宽度=10则一行的实际字节数=32=Math.ceil(10*3/4)*4，补了2字节的0
+     例如宽度=11则一行的实际字节数=36=Math.ceil(11*3/4)*4，补了3字节的0
+     算法： Math.ceil(宽度*每个像素占用的字节/4)*4*/
+    var lineByteNum = Math.ceil(w*3/4)*4; //每行数据的实际字节数
+    var dataLen = lineByteNum * h;
+
+    this.w = w;
+    this.h = h;
+    this.fileLen = dataLen + 54;
+    this._lineByteNum = lineByteNum;
+    this._data = new Buffer(this.fileLen).fill(0);
+
+    //BMP文件头初始化
+    makeHeader(this._data, this.fileLen, dataLen, w, h);
+}
+
+/*
+从文件加载bmp
+参数：文件路径 ， cb(err,bmp24)
+注意！必需确保文件是24位bmp
+ */
+BMP24.loadFromFile = function loadFromFile(filename, cb) {
+    fs.readFile(filename, function(err, data) {
+        if (err) return cb(err);
+        var img = new BMP24(0,0);
+        img.w = data.readInt32LE(18);
+        img.h = data.readInt32LE(22);
+        img.fileLen = data.length;
+        img._lineByteNum = Math.ceil(img.w*3/4)*4;
+        img._data = data;
+        cb(null, img);
+    });
+}
+
+//获取BMP整个文件数据
+BMP24.prototype.getFileData = function getFileData() {
+    return this._data;
+}
+
+//获取BMP图像数据
+BMP24.prototype.getData = function getData() {
+    return this._data.slice(54);
+}
+
+//画点
+BMP24.prototype.drawPoint = function drawPoint(x, y, rgb) {
+    if(x>=this.w || y>=this.h || x<0 || y<0){
+        return;
+    }
+    //数据第一行为图像的最底一行，颜色数据在十六进制编辑器中的排列是蓝绿红
+    var line = this.h - y - 1;//因为data的第一行是图片的最后一行，所以要反过来
+    var pos = 54 + (x*3) + (this._lineByteNum * line);
+    this._data.writeUInt8(        rgb & 0xFF, pos);   //蓝
+    this._data.writeUInt8( (rgb >> 8) & 0xFF, pos+1); //绿
+    this._data.writeUInt8((rgb >> 16) & 0xFF, pos+2); //红
+}
+
+//画水平线
+BMP24.prototype.drawLineH = function drawLineH(x1, x2, y, rgb) {
+    if (x1 > x2) {
+        var tmp = x2;
+        x2 = x1;
+        x1 = tmp;
+    }
+    for (; x1 <= x2; x1++){
+        this.drawPoint(x1, y, rgb);
+    }
+}
+
+BMP24.prototype.drawLineV = function drawLineV(y1, y2, x, rgb) {
+    if (y1 > y2) {
+        var tmp = y2;
+        y2 = y1;
+        y1 = tmp;
+    }
+    for (; y1 <= y2; y1++){
+        this.drawPoint(x, y1, rgb);
+    }
+}
+
+BMP24.prototype.drawLine = function drawLine(x1, y1, x2, y2, rgb) {
+    var t, distance;
+    var x = 0, y = 0, delta_x, delta_y;
+    var incx, incy;
+
+    delta_x = x2 - x1;
+    delta_y = y2 - y1;
+    if (delta_x > 0)
+        incx = 1;
+    else if (delta_x == 0) {
+        this.drawLineV(x1, y1, y2, rgb);
+        return;
+    } else
+        incx = -1;
+    if (delta_y > 0)
+        incy = 1;
+    else if (delta_y == 0) {
+        this.drawLineH(x1, x2, y1, rgb);
+        return;
+    }
+    else incy = -1;
+    delta_x = delta_x<0 ? -delta_x : delta_x;
+    delta_y = delta_y<0 ? -delta_y : delta_y;
+    if (delta_x > delta_y)
+        distance = delta_x;
+    else distance = delta_y;
+    this.drawPoint(x1, y1, rgb);
+    for (t = 0; t <= distance + 1; t++) {
+        this.drawPoint(x1, y1, rgb);
+        x += delta_x;
+        y += delta_y;
+        if (x > distance) {
+            x -= distance;
+            x1 += incx;
+        }
+        if (y > distance) {
+            y -= distance;
+            y1 += incy;
+        }
+    }
+}
+
+BMP24.prototype.drawRect = function fillRect(x1, y1, x2, y2, rgb) {
+    this.drawLineH(x1, x2, y1, rgb);
+    this.drawLineH(x1, x2, y2, rgb);
+    this.drawLineV(y1, y2, x1, rgb);
+    this.drawLineV(y1, y2, x2, rgb);
+}
+
+BMP24.prototype.fillRect = function fillRect(x1, y1, x2, y2, rgb) {
+    var x;
+    if(x1 > x2){
+        var tmp = x2;
+        x2 = x1;
+        x1 = tmp;
+    }
+    if(y1 > y2){
+        var tmp = y2;
+        y2 = y1;
+        y1 = tmp;
+    }
+    for(; y1 <= y2; y1++){
+        for(x=x1; x <= x2; x++){
+            this.drawPoint(x, y1, rgb);
+        }
+    }
+}
+
+//画圆，xy为中心点位置
+BMP24.prototype.drawCircle = function drawCircle(x, y, r, rgb) {
+    var a, b, c;
+    a = 0;
+    b = r;
+    //  c = 1.25 - r;
+    c = 3 - 2 * r;
+    while (a < b) {
+        this.drawPoint(x + a, y + b, rgb);
+        this.drawPoint(x - a, y + b, rgb);
+        this.drawPoint(x + a, y - b, rgb);
+        this.drawPoint(x - a, y - b, rgb);
+        this.drawPoint(x + b, y + a, rgb);
+        this.drawPoint(x - b, y + a, rgb);
+        this.drawPoint(x + b, y - a, rgb);
+        this.drawPoint(x - b, y - a, rgb);
+        if (c < 0) {
+            c = c + 4 * a + 6;
+        } else {
+            c = c + 4 * (a - b) + 10;
+            b -= 1;
+        }
+        a = a + 1;  //控制打点间隔
+    }
+    if (a == b) {
+        this.drawPoint(x + a, y + b, rgb);
+        this.drawPoint(x - a, y + b, rgb);
+        this.drawPoint(x + a, y - b, rgb);
+        this.drawPoint(x - a, y + b, rgb);
+        this.drawPoint(x + b, y + a, rgb);
+        this.drawPoint(x - b, y + a, rgb);
+        this.drawPoint(x + b, y - a, rgb);
+        this.drawPoint(x - b, y - a, rgb);
+    }
+}
+
+BMP24.prototype.drawChar = function drawChar(ch, x, y, font, color) {
+    var index = font.fonts.indexOf(ch);
+    if(index < 0) return;
+    var fontData = font.data[index];
+    var y0 = y;
+    var x0 = x;
+    for(var i=0; i<fontData.length; i++){
+        x0 = x;
+        for(var b=fontData[i]; b>0; b<<=1){
+            if (b & 0x80) {
+                this.drawPoint(x0, y0, color);
+            }
+            x0++;
+        }
+        y0++;
+        if((y0 - y) >= font.h){
+            y0 = y;
+            x+=8;
+        }
+    }
+}
+
+BMP24.prototype.drawString = function drawString(str, x, y, font, color) {
+    for(var i=0; i<str.length; i++){
+        this.drawChar(str[i], x, y, font, color);
+        x+=font.w;
+    }
+}
+
+
+var font = require('./font');
+
+BMP24.font8x16 = font.font8x16;
+BMP24.font12x24 = font.font12x24;
+BMP24.font16x32 = font.font16x32;
+
+exports = module.exports = BMP24;
+
+///////////////Copyright zengming 2010-2017/////////////////////////////////////////////
---
+++ demo/test.js
@@ -0,0 +1,103 @@
+var http = require('http');
+var fs = require('fs');
+var BMP24 = require('../BMP24');//gd-bmp
+
+/*
+ 用PCtoLCD2002取字模
+ 行列式扫描，正向取模（高位在前）
+ */
+var cnfonts = {//自定义字模
+    w : 16,
+    h : 16,
+    fonts: "中国",
+    data : [
+        [0x01,0x01,0x01,0x01,0x3F,0x21,0x21,0x21,0x21,0x21,0x3F,0x21,0x01,0x01,0x01,0x01,0x00,0x00,0x00,0x00,0xF8,0x08,0x08,0x08,0x08,0x08,0xF8,0x08,0x00,0x00,0x00,0x00],/*"中",0*/
+        [0x00,0x7F,0x40,0x40,0x5F,0x41,0x41,0x4F,0x41,0x41,0x41,0x5F,0x40,0x40,0x7F,0x40,0x00,0xFC,0x04,0x04,0xF4,0x04,0x04,0xE4,0x04,0x44,0x24,0xF4,0x04,0x04,0xFC,0x04],/*"国",1*/
+    ]
+};
+
+//测试字库
+function makeImg2() {
+    var img = new BMP24(300,140);
+    img.drawString('helloworld', 20,10, BMP24.font8x16, 0xff0000);
+    img.drawString('helloworld', 20,25, BMP24.font12x24, 0x00ff00);
+    img.drawString('helloworld', 20,50, BMP24.font16x32, 0x0000ff);
+    img.drawString('中国', 20,85, cnfonts, 0xffffff);
+    return img;
+}
+
+//仿PHP的rand函数
+function rand(min, max) {
+    return Math.random()*(max-min+1) + min | 0; //特殊的技巧，|0可以强制转换为整数
+}
+//制造验证码图片
+function makeCapcha() {
+    var img = new BMP24(100, 40);
+    img.drawCircle(11, 11, 10, rand(0, 0xffffff));
+    img.drawRect(0, 0, img.w-1, img.h-1, rand(0, 0xffffff));
+    img.fillRect(53, 15, 88, 35, rand(0, 0xffffff));
+    img.drawLine(50, 6, 3, 60, rand(0, 0xffffff));
+    //return img;
+
+    //画曲线
+    var w=img.w/2;
+    var h=img.h;
+    var color = rand(0, 0xffffff);
+    var y1=rand(-5,5); //Y轴位置调整
+    var w2=rand(10,15); //数值越小频率越高
+    var h3=rand(4,6); //数值越小幅度越大
+    var bl = rand(1,5);
+    for(var i=-w; i<w; i+=0.1) {
+        var y = Math.floor(h/h3*Math.sin(i/w2)+h/2+y1);
+        var x = Math.floor(i+w);
+        for(var j=0; j<bl; j++){
+            img.drawPoint(x, y+j, color);
+        }
+    }
+
+    var p = "ABCDEFGHIJKLMNOPQRSTUVWXYZ3456789";
+    var str = '';
+    for(var i=0; i<5; i++){
+        str += p.charAt(Math.random() * p.length |0);
+    }
+
+    var fonts = [BMP24.font8x16, BMP24.font12x24, BMP24.font16x32];
+    var x = 15, y=8;
+    for(var i=0; i<str.length; i++){
+        var f = fonts[Math.random() * fonts.length |0];
+        y = 8 + rand(-10, 10);
+        img.drawChar(str[i], x, y, f, rand(0, 0xffffff));
+        x += f.w + rand(2, 8);
+    }
+    return img;
+}
+
+
+
+//测试生成验证码的效率
+var start = Date.now();
+var i = 0;
+while((Date.now() - start) < 1000){
+    //var img = makeCapcha();
+    var img = makeImg2();
+    i++;
+}
+console.log("1秒钟生成：" + i);
+
+
+
+http.createServer(function (req,res) {
+	if(req.url == '/favicon.ico'){
+		return res.end();
+	}
+    console.time("bmp24");
+    var img = makeCapcha();
+	//var img = makeImg2();
+    console.timeEnd("bmp24");
+
+    res.setHeader('Content-Type', 'image/bmp');
+    res.end(img.getFileData());
+
+}).listen(3000);
+
+console.log('localhost:3000');
